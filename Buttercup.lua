f0c01743b7746f4391c7a1da7601037fe4479083
LuaGunsX <134158550+LuaGunsX@users.noreply.github.com>
2024-10-22 17:01:56 -0400

    Create Buttercup.lua

---
+++ Buttercup.lua
@@ -0,0 +1,146 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Buttercup Hub", "Ocean")
+
+--// universal section \\--
+
+local Tab = Window:NewTab("Universal")
+local KnifeSection = Tab:NewSection("Universal Scripts")
+
+
+KnifeSection:NewButton("Infinite Yield FE", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end)
+
+KnifeSection:NewButton("Asgore Color Changing Trident", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Asgore%20Color%20Changing%20Trident.lua"))()
+end)
+
+KnifeSection:NewButton("Bird Wings", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Bird%20Wings.txt", true))()
+end)
+
+KnifeSection:NewButton("Brack Hub 4.0", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Bow.txt", true))()
+end)
+
+KnifeSection:NewButton("c00lgui", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/C00lKid%20Gui.txt", true))()
+end)
+
+
+KnifeSection:NewButton("Dick Gun", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Dick_Gun.txt", true))()
+end)
+
+KnifeSection:NewButton("Dubstep Gun", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Dubstep%20Gun.txt", true))()
+end)
+
+KnifeSection:NewButton("Earrape GUI", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Earrape%20GUI.txt", true))()
+end)
+
+
+KnifeSection:NewButton("GMOD Error", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/GmodError.txt", true))()
+end)
+
+KnifeSection:NewButton("Grab Knife 1.0", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Grab%20Knife%20V1.txt", true))()
+end)
+
+KnifeSection:NewButton("Grab Knife 2.0", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Grab%20Knife%20V2.txt", true))()
+end)
+
+
+KnifeSection:NewButton("Grab Knife 3.0", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Grab%20Knife%20V3.txt", true))()
+end)
+
+KnifeSection:NewButton("Grab Knife 4.0", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Grab%20Knife%20V4.txt", true))()
+end)
+
+KnifeSection:NewButton("Omni God of Destruction", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Omni%20God%20of%20Destruction.txt", true))()
+end)
+
+
+KnifeSection:NewButton("Rainbow Wand", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Rainbow%20Wand.txt", true))()
+end)
+
+KnifeSection:NewButton("Rose Hub V3", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Rose%20Hub%20V3.txt", true))()
+end)
+
+KnifeSection:NewButton("Rose Hub V1", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/RoseHub1.1.3.txt", true))()
+end)
+
+
+KnifeSection:NewButton("Shoop Da Whoop", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/ShoopDahWhoop.txt", true))()
+end)
+
+KnifeSection:NewButton("Slappy Admin", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Slappy%20Admin.txt", true))()
+end)
+
+KnifeSection:NewButton("SlenderMe", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Slenderman.txt", true))()
+end)
+
+
+KnifeSection:NewButton("Snoop Dogg [Press R]", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/SnoopDogg%20(Press%20'R').txt", true))()
+end)
+
+KnifeSection:NewButton("Suicide Gun", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Suicide%20Gun.txt", true))()
+end)
+
+KnifeSection:NewButton("T0PK3K V2.0", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/T0PK3K%20V2.txt", true))()
+end)
+
+KnifeSection:NewButton("T0PK3K V3.0", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/T0PK3K%20V3.txt", true))()
+end)
+
+KnifeSection:NewButton("The Flash", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/The%20Flash.txt", true))()
+end)
+
+KnifeSection:NewButton("Thomas the Dank Engine", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Thomas%20the%20dank%20engine.txt", true))()
+end)
+
+KnifeSection:NewButton("v3rmillion Tabs", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/V3rmillion%20Tabs.txt", true))()
+end)
+
+KnifeSection:NewButton("Van GUI", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Van%20GUI.txt", true))()
+end)
+
+KnifeSection:NewButton("WYD Form", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/WYD%20Form.txt", true))()
+end)
+
+KnifeSection:NewButton("Wheelchair", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Wheelchair.txt", true))()
+end)
+
+KnifeSection:NewButton("Whip", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/Whip.lua", true))()
+end)
+
+KnifeSection:NewButton("YourMom", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/YourMom%20Gui.txt", true))()
+end)
+
+KnifeSection:NewButton("iOrb 2.0", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaGunsX/ButtercupHub/refs/heads/main/Data/iOrb%20V2.lua", true))()
+end)
